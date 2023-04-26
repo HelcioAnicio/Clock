b75338bbe5fb458503833116232a79e2997d3aa1
Helcio <helcioanicio19@gmail.com>
2023-04-25 22:29:37 -0300

    Used JS to improve the project with automatic functionality

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
-console.log('quero alguma coisa')
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.min');
+const second = document.querySelector('.sec');
+
+function time() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    
+    hours < 10 ? hour.innerHTML = `0${hours}` : hour.innerHTML = hours
+    minutes < 10 ? minute.innerHTML = `0${minutes}` : minute.innerHTML = minutes
+    seconds < 10 ? second.innerHTML = `0${seconds}` : second.innerHTML = seconds
+
+    setTimeout('time()', 500);
+}
